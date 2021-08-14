14956e2bb693f3d586ad37e5375671ea9509ff2c
Hyeonjin Jeong <bangawork7@gmail.com>
2021-08-14 18:00:59 +0900

    Header 수정

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -11,18 +11,35 @@ const AppRouter = ({ refreshUser, isLoggedIn, userObj }) => {
                         <Route exact path="/">
                             <Main userObj={userObj}/>
                         </Route>
-                        <Route path="/metabus">
+                        <Route path="/signin">
+                            <SignIn userObj={userObj}/>
+                            {/* <Main userObj={userObj}/> */}
+                        </Route>
+                        <Route path="/signUp">
+                            <SignUp userObj = {userObj}/>
+                            </Route>
+                        <Redirect from="*" to="/"/>
+                        <Route path="/recruit">
+                            <MetaBus userObj = {userObj}/>
+                        </Route>
+                        <Route path="/board">
+                            <MetaBus userObj = {userObj}/>
+                        </Route>
+                        <Route path="/gallery">
                             <MetaBus userObj = {userObj}/>
                         </Route>
                         <Route path="/calendar">
                             <Calendar userObj = {userObj}/>
                         </Route>
+                        <Route path="/tour">
+                            <MetaBus userObj = {userObj}/>
+                        </Route>
                     </>
                 ) : (
                     <>
-                        <Route exact path="/">
-                            {/* <SignIn userObj={userObj}/> */}
-                            <Main userObj={userObj}/>
+                        <Route path="/signin">
+                            <SignIn userObj={userObj}/>
+                            {/* <Main userObj={userObj}/> */}
                         </Route>
                         <Route path="/signUp">
                             <SignUp userObj = {userObj}/>
