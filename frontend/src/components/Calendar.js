7d80888f39674979d31e7dab890fd2f4538ffb9a
한상현 <luck2901@naver.com>
2021-08-14 19:04:22 +0900

    Update/router, login, calendar

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin  from '@fullcalendar/daygrid';
 
 function Calendar(props) {
   return (
-    <div>
-      캘린더 
+    <div className='mypage-body'>
+      <div className='body-wrapper box'>
+        <div className='body-info-container'>
+          <div className='calendar-wrapper'>
+            <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+const SignInBlock = styled.div`
+  calendar-wrapper {
+    height:500
+  }
+
+`;
 
 export default Calendar;
