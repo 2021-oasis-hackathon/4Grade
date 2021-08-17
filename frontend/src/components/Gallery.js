0cb6c2c4385775140f984366deaa6f9ecbdf8f15
한상현 <luck2901@naver.com>
2021-08-18 00:32:23 +0900

    Update/Main

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -1,8 +1,6 @@
 import React, { Component, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"
+
 import { Button, Spinner } from "react-bootstrap";
 import GalleryPicture from './GalleryPicture';
 import { AiOutlineSearch } from 'react-icons/ai'
